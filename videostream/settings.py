a2beaaca4782415e84fc2a132e70ec713b53a224
= <=>
2025-10-21 09:30:38 -0700

    allowed host

--- videostream/settings.py
+++ videostream/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-video-streaming-app-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.4', '*']
+ALLOWED_HOSTS = ['coder-47ty.onrender.com*', 'localhost', '127.0.0.1', ]
 
 # Application definition
 INSTALLED_APPS = [
